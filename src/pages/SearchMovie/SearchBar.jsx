3bd11f3318b362a63e5f0d8cf62ba1de5ef40bb6
GMKRAKEN23 <[messinagiuseppe9423@gmail.com]>
2024-05-12 19:38:53 +0200

    modif search

--- src/pages/SearchMovie/SearchBar.jsx
+++ src/pages/SearchMovie/SearchBar.jsx
@@ -1,9 +1,14 @@
 function SearchBar() {
     return (
-        <form className="max-w-lg mx-auto my-5">
+        <form className="max-w-lg mx-auto mt-10">
             <div className="relative w-3/4 mx-auto">
-                <input type="search" id="search-dropdown" className="block p-2.5 w-full regular tracking-wider z-20 text-sm bg-white rounded-lg border focus:border-white bg-white placeholder-black" placeholder="Search your best movie" required />
-                <button type="submit" className="absolute top-0 end-0 p-2.5 text-white font-medium h-full rounded-lg  hover:bg-orange-700 focus:outline-none bg-orange-600">
+                <input 
+                type="search" 
+                className="block p-2.5 w-full regular tracking-wider z-20 text-base bg-white rounded-lg border bg-white placeholder-black" 
+                placeholder="Search your best movie" 
+                required 
+                />
+                <button type="submit" className="absolute top-0 end-0 p-2.5 text-white font-medium h-full rounded-e-lg  hover:bg-orange-700 focus:outline-none bg-orange-600">
                     <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                     </svg>
--- src/pages/Watchlist/WatchList.jsx
+++ src/pages/Watchlist/WatchList.jsx
@@ -3,7 +3,7 @@ import NavBarBottom from "../../components/NavBarBottom/NavBarBottom";
 function WatchList(){
     return (
         <div className="w-full flex h-20 cursor-pointer justify-center items-center">
-            <h2 className="lato text-white text-xl tracking-widest font-semibold text-center">Watch list</h2>  
+            <h2 className="lato text-white text-2xl tracking-widest font-semibold text-center">Watchlist</h2>  
             <NavBarBottom />
         </div>
       
