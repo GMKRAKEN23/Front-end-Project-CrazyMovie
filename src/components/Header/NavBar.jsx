7dbd1f9b09735f662563d57d53565708fda16d25
GMKRAKEN23 <[messinagiuseppe9423@gmail.com]>
2024-05-14 16:28:19 +0200

    modif

--- src/components/Header/NavBar.jsx
+++ src/components/Header/NavBar.jsx
@@ -4,15 +4,15 @@ function NavBar({ active }) {
     return (
         <div className={`${active ? "block" : "hidden"} absolute h-full top-32 bg-black w-full z-10`}>
             <ul className="list-none flex flex-col items-stard">
-                <Link><li className="text-white text-2xl mb-3 pl-10 hover:text-orange-600">Action</li></Link>
-                <Link><li className="text-white text-2xl my-3 pl-10 hover:text-orange-600">Comedy</li></Link>
-                <Link><li className="text-white text-2xl my-3 pl-10 hover:text-orange-600">Family</li></Link>      
-                <Link><li className="text-white text-2xl my-3 pl-10 hover:text-orange-600">Animation</li></Link>          
-                <Link><li className="text-white text-2xl my-3 pl-10 hover:text-orange-600">Fantasy</li></Link>
-                <Link><li className="text-white text-2xl my-3 pl-10 hover:text-orange-600">Drama</li></Link>
-                <Link><li className="text-white text-2xl my-3 pl-10 hover:text-orange-600">Thriller</li></Link>
-                <Link><li className="text-white text-2xl my-3 pl-10 hover:text-orange-600">Adventure</li></Link>
-                <Link><li className="text-white text-2xl my-3 pl-10 hover:text-orange-600">Science Fiction</li></Link>
+                <Link><li className="text-white text-2xl mb-3 pl-10">Action</li></Link>
+                <Link><li className="text-white text-2xl my-3 pl-10">Comedy</li></Link>
+                <Link><li className="text-white text-2xl my-3 pl-10">Family</li></Link>      
+                <Link><li className="text-white text-2xl my-3 pl-10">Animation</li></Link>          
+                <Link><li className="text-white text-2xl my-3 pl-10">Fantasy</li></Link>
+                <Link><li className="text-white text-2xl my-3 pl-10">Drama</li></Link>
+                <Link><li className="text-white text-2xl my-3 pl-10">Thriller</li></Link>
+                <Link><li className="text-white text-2xl my-3 pl-10">Adventure</li></Link>
+                <Link><li className="text-white text-2xl my-3 pl-10">Science Fiction</li></Link>
             </ul>
 
         </div>
