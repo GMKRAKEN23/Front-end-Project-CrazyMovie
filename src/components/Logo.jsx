900fa08f36163ffe555e6de6aa98fc25b9beecb3
GMKRAKEN23 <[messinagiuseppe9423@gmail.com]>
2024-05-17 15:45:44 +0200

    modif path logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ function Logo(){
     return (
         <>
         <Link to="/homepage" className="cursor-pointer">
-            <img src="./assets/logo.png" className="w-[125px] h-[125px]" alt="logo_crazy_movie" />
+            <img src="/assets/logo.png" className="w-[125px] h-[125px]" alt="logo_crazy_movie" />
         </Link>
         </>
     )
