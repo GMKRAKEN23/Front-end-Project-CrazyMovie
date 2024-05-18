8407e9d91b89fbd67392d0ae9e8d4a3318e79503
GMKRAKEN23 <[messinagiuseppe9423@gmail.com]>
2024-05-18 15:52:10 +0200

    add link for movie search bar

--- src/pages/SearchMovie/SearchBar.jsx
+++ src/pages/SearchMovie/SearchBar.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function SearchBar() {
     const [searchTerm, setSearchTerm] = useState("");
@@ -16,7 +17,7 @@ function SearchBar() {
                 throw new Error('Error searching for movies');
             }
             const data = await response.json();
-            const filteredResults = data.results.filter(movie => movie.poster_path); 
+            const filteredResults = data.results.filter(movie => movie.poster_path);
             setMovieResults(filteredResults);
         } catch (error) {
             console.error('Error searching for movies', error);
@@ -26,12 +27,12 @@ function SearchBar() {
     return (
         <form onSubmit={handleSearchSubmit} className="max-w-lg mx-auto mt-10">
             <div className="relative w-3/4 mx-auto">
-                <input 
-                    type="search" 
+                <input
+                    type="search"
                     value={searchTerm}
                     onChange={handleSearchChange}
-                    className="block p-2.5 w-full regular tracking-wider z-20 text-base bg-white rounded-lg border bg-white placeholder-black" 
-                    placeholder="Search your best movie" 
+                    className="block p-2.5 w-full regular tracking-wider z-20 text-base bg-white rounded-lg border bg-white placeholder-black"
+                    placeholder="Search your best movie"
                 />
                 <button type="submit" className="absolute top-0 end-0 p-2.5 text-white font-medium h-full rounded-e-lg  hover:bg-orange-700 focus:outline-none bg-orange-600">
                     <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
@@ -41,11 +42,17 @@ function SearchBar() {
             </div>
             <div className="grid w-3/4 grid-cols-2 gap-4 mx-auto mt-4">
                 {movieResults.map((movie) => (
-                    <div key={movie.id} className="flex flex-col items-center">
-                        <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} className="mt-4 rounded-lg cursor-pointer w-[10em] h-[10em]" />
-                        <p className="mt-2 tracking-wider text-center text-white">{movie.title}</p>
-                        <p className="text-orange-600">({new Date(movie.release_date).getFullYear()})</p>
-                    </div>
+                    <Link to={`/detailsmovies/${movie.id}`} key={movie.id}>
+                        <div className="flex flex-col items-center">
+                            <img
+                                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                                alt={movie.title}
+                                className="mt-4 rounded-lg cursor-pointer w-[10em] h-[10em]"
+                            />
+                            <p className="mt-2 tracking-wider text-center text-white">{movie.title}</p>
+                            <p className="text-orange-600">({new Date(movie.release_date).getFullYear()})</p>
+                        </div>
+                    </Link>
                 ))}
             </div>
         </form>
