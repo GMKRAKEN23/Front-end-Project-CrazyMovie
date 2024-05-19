9bc7890eacf1f36af6ad7634811091541be291d6
GMKRAKEN23 <[messinagiuseppe9423@gmail.com]>
2024-05-19 22:30:01 +0200

    add folder loader and file

---
+++ src/pages/Loader/TrendingLoader.js
@@ -0,0 +1,11 @@
+import { fetchTrendingMovies } from '../api/trendingApi'; 
+
+export async function trendingLoader() {
+  try {
+    const movies = await fetchTrendingMovies();
+    return { movies };
+  } catch (error) {
+    console.error('Error while recovering trending movies:', error);
+    throw error;
+  }
+}
