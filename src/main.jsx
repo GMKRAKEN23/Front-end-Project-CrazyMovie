6b58c3782eb72d0227837f1ea7711e1a339370b2
GMKRAKEN23 <[messinagiuseppe9423@gmail.com]>
2024-05-19 13:23:05 +0200

    modif

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { RouterProvider } from 'react-router'
+import { RouterProvider } from 'react-router-dom'
 import { router } from '../src/router'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
