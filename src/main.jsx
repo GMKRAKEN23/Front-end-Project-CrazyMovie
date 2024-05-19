d6a18d7d17e00409791f88642aa075df75b0b622
GMKRAKEN23 <[messinagiuseppe9423@gmail.com]>
2024-05-19 14:26:34 +0200

    Votre message de commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import { router } from '../src/router'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import { router } from '../src/router';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-    </RouterProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
