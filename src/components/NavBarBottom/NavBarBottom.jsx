31fc96fc74ebf14051ea0bd5dda32ee54a84bcac
GMKRAKEN23 <[messinagiuseppe9423@gmail.com]>
2024-05-21 14:47:36 +0200

    delete

--- src/components/NavBarBottom/NavBarBottom.jsx
+++ src/components/NavBarBottom/NavBarBottom.jsx
@@ -10,7 +10,7 @@ function NavBarBottom() {
     };
 
     return ( 
-        <div className="fixed bottom-0 z-50 w-full h-16 -translate-x-1/2 bg-white border border-gray-600 left-1/2 ">
+        <div className="fixed bottom-0 z-50 w-full h-16 -translate-x-1/2 bg-white border border-gray-600 left-1/2 max-screen">
             <div className="grid h-full grid-cols-3 mx-auto">
                 <button data-tooltip-target="tooltip-home" type="button" className="inline-flex flex-col items-center justify-center px-5">
                     <Link to="/homepage"><i className={`bx bxs-home-alt-2 text-4xl cursor-pointer ${location.pathname === "/" || location.pathname === "/homepage" ? "text-orange-600" : ""}`}></i></Link>
